cb6dad5f98ae27068f88adcd543e61945c729b2f
AlexeyGrk <alexgmirko@gmail.com>
2021-09-16 23:17:48 +0300

    add all fn-s model

--- model/index.js
+++ model/index.js
@@ -1,20 +1,51 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises')
+const path = require('path')
+const contacts = require('./contacts.json')
+const filePath = path.join(__dirname, './contacts.json')
+const listContacts = async () => contacts
 
-const listContacts = async () => {}
+const getContactById = async (contactId) => {
+  const contactsList = await listContacts()
+  console.log(filePath)
+  const contact = contactsList.find((item) => item.id === contactId)
 
-const getContactById = async (contactId) => {}
+  if (!contact) {
+    console.log('Not found contact')
+    return null
+  }
+  return contact
+}
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contactsList = await listContacts()
+  const idx = contactsList.findIndex((item) => item.id === contactId)
+  if (idx === -1) {
+    console.log('Not found contact')
+    return null
+  }
+  contactsList.splice(idx, 1)
+  await updateContacts(contactsList)
+  return 'Success remove'
+}
 
-const addContact = async (body) => {}
+const addContact = async ({ name, phone, email, id }) => {
+  const contactsList = await listContacts()
+  const newContact = { name, phone, email, id }
 
-const updateContact = async (contactId, body) => {}
+  const newProducts = [...contactsList, newContact]
+
+  await updateContacts(newProducts)
+  return newContact
+}
+
+const updateContacts = async (newContacts) => {
+  await fs.writeFile(filePath, JSON.stringify(newContacts))
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
+  updateContacts,
 }
